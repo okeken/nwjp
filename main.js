ae06a0f8eca272437cefa858941a9f096a8469b3
okeken <55279370+okeken@users.noreply.github.com>
2020-03-02 21:01:25 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+$('.owl-carouse2').owlCarousel({
+  loop: true,
+  autoplay: false,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    560: {
+      items: 1
+    }
+  }
+});
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  autoplay: true,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    560: {
+      items: 2
+    }
+  }
+});
